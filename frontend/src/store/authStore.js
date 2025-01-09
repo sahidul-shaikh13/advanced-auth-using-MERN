bfb6b60fbe38ac5507dd2d76e6397990bc2f7351
Sahidul <foru51813@gmail.com>
2025-01-10 01:16:34 +0530

    URL updated in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand"
 import axios from "axios"
 
 
-const API_URL = "http://localhost:8000/api/auth" 
+const API_URL = import.meta.env.MODE  === "development" ? "http://localhost:8000/api/auth" : "/api/auth";
 
 
 
